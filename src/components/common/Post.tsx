b646761484a1e6a0492dc471caab89c017b60028
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2023-12-15 22:07:13 +0100

    add posts with comments and users

--- src/components/common/Post.tsx
+++ src/components/common/Post.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from "react";
 import { PostType } from "../types/Post.type";
 import { UserType } from "../types/User.type";
+import { CommentType } from "../types/Comment.type";
 
-type MergedPostType = PostType & { user: UserType };
+type MergedPostType = PostType & { user: UserType; comments: CommentType[] };
 interface Props{
     post : MergedPostType;
     index : number;
@@ -12,9 +13,16 @@ const Post:FC<Props> = ({post,index}) =>{
     
     return(
             <>
-                <hr></hr>
-                {post.user.username}
-                {post.body}
+                <h1>Autor: {post.user.username}</h1>
+                <h2>Post body: {post.body}</h2>
+                
+                {post.comments.map((comment) => (
+                <div key={comment.id}>
+                    Komentujacy: <b>{comment.name} </b><br></br>
+                    Koment body: {comment.email}
+                    <hr></hr>
+                </div>
+                ))}
                 <br></br>
             </>  
     )
--- src/components/routes/Posts.tsx
+++ src/components/routes/Posts.tsx
@@ -4,48 +4,56 @@ import Post from "../common/Post";
 import "../style/Posts.css";
 import { PostType } from "../types/Post.type";
 import { UserType } from "../types/User.type";
+import { CommentType } from "../types/Comment.type";
 
 export const Posts = () => {
-    const [posts, setPosts] = useState<PostType[]>([]);
-    const [error, setError] = useState([]);
-    const [users, setUsers] = useState<UserType[]>([]);
-    
-    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => setUsers(json))
-        .catch((err) => setError(err));
-  
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((json) => setPosts(json))
-        .catch((err) => setError(err));
-    }, []);
-  
-    type MergedPostType = PostType & { user: UserType };
-  
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const [error, setError] = useState([]);
+  const [users, setUsers] = useState<UserType[]>([]);
+  const [comments, setComments] = useState<CommentType[]>([]);
 
-    const mergePostsAndUsers = (): MergedPostType[] => {
-      return posts.map((post) => {
-        const user = users.find((u) => u.id === post.userId);
-        return {
-          ...post,
-          user: user || ({} as UserType) 
-        };
-      });
-    };
-  
-    const postsWithUsers = mergePostsAndUsers();
-  
-    return (
-      <div className="posts-container">
-        {posts.length === 0 ? (
-          <Loader />
-        ) : (
-          postsWithUsers.map((post, index) => (
-            <Post key={post.id} index={index} post={post} />
-          ))
-        )}
-      </div>
-    );
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+      .catch((err) => setError(err));
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((err) => setError(err));
+
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => response.json())
+      .then((json) => setComments(json))
+      .catch((err) => setError(err));
+  }, []);
+
+  type MergedPostType = PostType & { user: UserType; comments: CommentType[] };
+
+  const mergePostsUsersComment = (): MergedPostType[] => {
+    return posts.map((post) => {
+      const user = users.find((u) => u.id === post.userId);
+      const postComments = comments.filter((comment) => comment.postId === post.id);
+      return {
+        ...post,
+        user: user || ({} as UserType),
+        comments: postComments || [],
+      };
+    });
   };
+
+  const postsWithUsers = mergePostsUsersComment();
+
+  return (
+    <div className="posts-container">
+      {posts.length === 0 ? (
+        <Loader />
+      ) : (
+        postsWithUsers.map((post, index) => (
+          <Post key={post.id} index={index} post={post} />
+        ))
+      )}
+    </div>
+  );
+};
